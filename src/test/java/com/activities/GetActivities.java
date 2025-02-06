e21922eec51be176cd052aa3441a62a11342dfff
SujataSonawane4001 <sujata62sonawane@gmail.com>
2025-02-07 02:25:11 +0530

    api for activities

--- src/test/java/com/activities/GetActivities.java
+++ src/test/java/com/activities/GetActivities.java
@@ -1,5 +1,38 @@
 package com.activities;
 
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
 public class GetActivities {
+
+	static String baseURL = "https://fakerestapi.azurewebsites.net/api/v1/Activities";
+
+	@Test
+	public void testGetActivities() {
+		given().when().get("https://fakerestapi.azurewebsites.net/api/v1/Activities").then().log().all()
+				.statusCode(200);
+
+	}
+
+	@Test
+	public void verifyStatusCode() {
+	 Response response=	given()
+		.when()
+		.get("https://fakerestapi.azurewebsites.net/api/v1/Activities");
+	   
+		assertEquals(200,response.statusCode(),"Expected status code is 200");
+
+	}
+
+	public void verifyResponseAndData() {
+		Response response=given()
+		.when()
+		.get("https://fakerestapi.azurewebsites.net/api/v1/Activities");
+		
+	}
 
 }
